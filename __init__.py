8510795ee717632a156a1937c23627b7f292881a
yossq236 <yf0659@gmail.com>
2026-04-07 05:06:35 +0900

    Add Flux Klein nodes

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from typing import override
 from comfy_api.latest import ComfyExtension, io
-from .node import FluxReferenceConditioningNode
+from .node import FluxReferenceConditioningNode, FluxKleinReferenceConditioningNode
 
 class MyExtension(ComfyExtension):
     @override
     async def get_node_list(self) -> list[type[io.ComfyNode]]:
-        return [FluxReferenceConditioningNode]
+        return [FluxReferenceConditioningNode,FluxKleinReferenceConditioningNode]
     @override
     async def on_load(self):
         pass
--- node.py
+++ node.py
@@ -15,6 +15,35 @@ class FluxReferenceConditioningNode(io.ComfyNode):
             display_name="Flux Reference Conditioning",
             category="advanced/conditioning",
             inputs=[
+                io.Conditioning.Input("conditioning"),
+                io.Vae.Input("vae"),
+                io.Autogrow.Input("images",template=image_template,tooltip="Images are available as image1-10"),
+                ],
+            outputs=[
+                io.Conditioning.Output("conditioning"),
+                ]
+        )
+
+    @classmethod
+    def execute(cls, conditioning, vae, images: io.Autogrow.Type) -> io.NodeOutput:
+        latent_list = [vae.encode(v) for v in images.values() if v is not None]
+        conditioning = node_helpers.conditioning_set_values(conditioning, {"reference_latents": latent_list}, append=True)
+        return io.NodeOutput(conditioning)
+
+class FluxKleinReferenceConditioningNode(io.ComfyNode):
+
+    @classmethod
+    def define_schema(cls) -> io.Schema:
+        image_template = io.Autogrow.TemplateNames(
+            io.Image.Input("image"),
+            names=[f"image{n}" for n in range(1,11)],
+            min=1,
+        )
+        return io.Schema(
+            node_id="FluxKleinReferenceConditioningNode",
+            display_name="Flux Klein Reference Conditioning",
+            category="advanced/conditioning",
+            inputs=[
                 io.Conditioning.Input("positive"),
                 io.Conditioning.Input("negative"),
                 io.Vae.Input("vae"),
